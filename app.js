e6106cb906bf56a7628cd954c5d23a82f0208900
terance98 <thomasterance98@gmail.com>
2020-01-30 10:34:13 +0530

    The first commit. Files are getting uploaded

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+var app = express();
+var upload = require('express-fileupload');
+
+app.use(upload()); // configure middleware
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+})
+app.post('/upload',function(req,res){
+  console.log(req.files);
+  if(req.files.upfile){
+    var file = req.files.upfile,
+      name = file.name,
+      type = file.mimetype;
+    var uploadpath = __dirname + '/uploads/' + name;
+    file.mv(uploadpath,function(err){
+      if(err){
+        console.log("File Upload Failed",name,err);
+        res.send("Error Occured!")
+      }
+      else {
+        console.log("File Uploaded",name);
+        res.send('Done! Uploading files')
+      }
+    });
+  }
+  else {
+    res.send("No File selected !");
+    res.end();
+  };
+})
+
+app.listen(4000, () => console.log(`Example app listening on port 4000!`));
